5ec06118427013137584038f4ca7cac7019cdb11
Benjamin Allan <baallan@sandia.gov>
2021-07-28 12:36:24 -0600

    fixes #772 missing unlock

--- ldms/src/core/ldms_xprt.c
+++ ldms/src/core/ldms_xprt.c
@@ -1681,7 +1681,7 @@ static int __process_dir_set_info(struct ldms_set *lset, enum ldms_dir_type type
 				ldms_dir_set_t dset, json_entity_t info_list)
 {
 	json_entity_t info_entity, e;
-	int j, rc;
+	int j, rc = 0;
 	int dir_upd = 0;
 	struct ldms_set_info_pair *pair, *nxt_pair;
 
@@ -1690,20 +1690,25 @@ static int __process_dir_set_info(struct ldms_set *lset, enum ldms_dir_type type
 	for (j = 0, info_entity = json_item_first(info_list); info_entity;
 	     info_entity = json_item_next(info_entity), j++) {
 		e = json_value_find(info_entity, "key");
-		dset->info[j].key = strdup(json_value_str(e)->str);
-		if (!dset->info[j].key)
-			return ENOMEM;
 		e = json_value_find(info_entity, "value");
-		dset->info[j].value = strdup(json_value_str(e)->str);
-		if (!dset->info[j].value)
-			return ENOMEM;
+		char *nkey = strdup(json_value_str(e)->str);
+		char *nvalue = strdup(json_value_str(e)->str);
+		if (!nkey || !nvalue) {
+			free(nkey);
+			free(nvalue);
+			rc = ENOMEM;
+			goto err;
+		}
+		dset->info[j].key = nkey;
+		dset->info[j].value = nvalue;
 
 		if (lset) {
 			const char *key = dset->info[j].key;
 			const char *val = dset->info[j].value;
 			rc = __ldms_set_info_set(&lset->remote_info, key, val);
-			if (rc > 0)
-				return rc;
+			if (rc > 0) {
+				goto err;
+			}
 			if (rc == 0)
 				dir_upd = 1;
 		}
@@ -1726,8 +1731,12 @@ static int __process_dir_set_info(struct ldms_set *lset, enum ldms_dir_type type
 		}
 		pair = nxt_pair;
 	}
+err:
 	if (lset) {
 		pthread_mutex_unlock(&lset->lock);
+		if (rc) {
+			return rc;
+		}
 		if ((type == LDMS_DIR_UPD) && dir_upd &&
 				(lset->flags & LDMS_SET_F_PUBLISHED)) {
 			__ldms_dir_upd_set(lset);
